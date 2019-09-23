d9f3c8c1c72bfbee321400425e28e571530986b1
Sekklow <sekklowz@gmail.com>
2019-09-23 13:01:34 +0300

    Improved documentation

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -13,14 +13,25 @@ sudoku_matrix = [  # 0 - means empty cell
     [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ]
 
-qwidth  = 3               # Quadrant Width
-qheight = 3               # Quadrant Height
-nums    = list(range(1,10))  # Possible Numbers
+qwidth  = 3                   # Quadrant Width
+qheight = 3                   # Quadrant Height
+nums    = list(range(1, 10))  # Possible Numbers
 
 
 def get_quadrant(matrix, row, col):
-    """Get all elements in a cell's quadrant."""
-    numbers   = nums[:]
+    """Get a submatrix of the cells quadrant.
+
+    Retrieves a 3x3 submatrix, containing all cells in 
+    a cell's quadrant.
+
+    Args:
+        int row: Index of the target cell's row.
+        int col: Index of the target cell's col.
+
+    Returns:
+        list<int>: 3x3 matrix, containing all cells in
+        the quadrant.
+    """
     col_start = qheight * (col // qheight)
     col_end   = col_start + qheight
     row_start = qwidth * (row // qwidth)
@@ -30,7 +41,18 @@ def get_quadrant(matrix, row, col):
 
 
 def get_quadrant_options(matrix, row, col):
-    """Get all available options in a cell's quadrant."""
+    """Retrieves a 3x3 submatrix, containing the valid sudoku
+    numbers in a cell's quadrant as per the rule that numbers
+    in a quadrant must not repeat.
+
+    Args:
+        int row: Index of the target cell's row.
+        int col: Index of the target cell's col.
+
+    Returns:
+        list<int>: 3x3 matrix, containing the valid numbers in a
+        a cell's quadrant.
+    """
     numbers    = nums[:]
     sub_matrix = get_quadrant(matrix, row, col)
     for row in sub_matrix:
@@ -41,7 +63,14 @@ def get_quadrant_options(matrix, row, col):
 
 
 def get_row_options(matrix, row):
-    """Get all available numbers in a row."""
+    """Retrieves all unoccupied options in a sudoku matrix row
+
+    Args:
+        int row: Index of the target row.
+
+    Returns:
+        list<int>: List of number 1-9 that are unoccupied in this row.
+    """
     numbers = nums[:]
     for item in matrix[row]:
         if item in numbers:
@@ -50,7 +79,14 @@ def get_row_options(matrix, row):
 
 
 def get_col_options(matrix, col):
-    """Get all available numbers in a column."""
+    """Retrieves all unoccupied options in a sudoku matrix col.
+
+    Args:
+        int col: Index of the target col.
+
+    Returns:
+        int array of unoccupied numbers.
+    """
     numbers = nums[:]
     for row in matrix:
         if row[col] in numbers:
@@ -59,8 +95,14 @@ def get_col_options(matrix, col):
 
 
 def intersected_items(*args):
-    """Returns items, present in all iterables.
-    param args - arbitrary number of python lists."""
+    """Filters elements, present in all collections.
+
+    Args:
+        list: List of elements. You could pass multiple lists to this function.
+
+    Returns:
+        list: Items present in all argument lists.
+    """
     intersection = []
     for item in args[0]:
         for item_set in args[1:]:
@@ -72,8 +114,15 @@ def intersected_items(*args):
 
 
 def find_next_empty_cell(matrix):
-    """Returns the index of the first cell with
-    a values of zero from the 2D matrix."""
+    """Find an empty cell in a sudoku matrix.
+
+    Args:
+        list<int> matrix: 9x9 sudoku matrix.
+
+    Returns:
+        list<int>[2]: [row_index, col_index] of the first free cell found.
+        -1: There's no empty space in the matrix.
+    """
     for row in range(9): 
         for col in range(9): 
             if(matrix[row][col]==0):
@@ -82,13 +131,19 @@ def find_next_empty_cell(matrix):
 
 
 def print_matrix(matrix):
-    """Renders the matrix to the console, using
-    dividers to separate quadrants."""
-    for ix,x in enumerate(matrix):
+    """Prints out a sudoku matrix on the console.
+
+        Args:
+            list<int>: 9x9 sudoku matrix.
+
+        Returns:
+            void
+        """
+    for ix, x in enumerate(matrix):
         if not (ix % 3):
             print("-"*(len(x)+4))
         out_line = []
-        for iy,y in enumerate(x):
+        for iy, y in enumerate(x):
             if (iy == 0) or (iy == len(x)) or not (iy % 3):
                 out_line.append("|")
             out_line.append(str(y))
@@ -98,8 +153,22 @@ def print_matrix(matrix):
 
 
 def get_valid_opts(matrix, row, col):
-    """Get the valid options for a cell in a
-    sudoku matrix."""
+    """Get the valid sudoku options(numbers) for a given cell.
+
+    Retrieves all possible numbers 1-9 for a given sudoku cell
+    based on the following rules:
+        1) Numbers must be unique in each row
+        2) Numbers must be unique in each column
+        3) Numbers must be unique in each quadrant(3x)
+
+    Args:
+        int row: Index of the target cell's row.
+        int col: Index of the target cell's col.
+
+    Returns:
+        list<int>: All numbers 1-9 that are valid for the
+        sudoku cell.
+    """
     row_opts  = get_row_options(matrix, row)
     col_opts  = get_col_options(matrix, col)
     quad_opts = get_quadrant_options(matrix, row, col)
@@ -107,17 +176,39 @@ def get_valid_opts(matrix, row, col):
 
 
 def check_sudoku(matrix):
+    """Check a partially filled sudoku matrix for a solution.
+
+    Recursively attempts to fill a sudoku matrix using a backtracking
+    algorithm. Potential options are narrowed down as per the sudoku
+    rules. If a solution is found it's printed on the console.
+
+    Args:
+        list<int> matrix: 9x9 Sudoku matrix. Some numbers can be pre-filled
+        and empty cells are marked as 0.
+
+    Returns:
+        bool: Result - True if a solution is found and False if no solution
+        can be found.
+    """
+    # Check if there are any free cells(free = 0).
     next_cell = find_next_empty_cell(matrix)
-    
+
+    # In case all cells are filled a solution is found
     if not next_cell:
         return True
 
-    row,col = next_cell
+    row, col = next_cell
+
+    # Retrieve all seemingly valid options for a cell
     opts    = get_valid_opts(matrix, row, col)
     for opt in opts:
+        # Make a prediction for a valid cell value and test it
         matrix[row][col] = opt
         if check_sudoku(matrix):
             return True
+
+        # In case the predicted value cannot yield a solution
+        # clear the prediction
         matrix[row][col] = 0
 
     return False
